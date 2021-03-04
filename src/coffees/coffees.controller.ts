34074c8946e7153fa811be65cb22c1958b94dad1
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-04 23:48:40 +0700

    Update

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,30 +1,37 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
+  @Get()
+  findAll(): string {
+    return 'List of all coffees';
+  }
 
-    @Get()
-    findAll(): string {
-        return 'List of all coffees';
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action return #${id} coffee.`;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return `This action return #${id} coffee.`;
-    }
+  @Post()
+  create(@Body() body: any): any {
+    return body;
+  }
 
-    @Post()
-    create(@Body() body: any): any {
-        return body;
-    }
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() body: any): string {
+    return `This action updates #${id} coffee.`;
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: number, @Body() body: any): string {
-        return `This action updates #${id} coffee.`;
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: number): string {
-        return `This action removes #${id} coffee.`;
-    }
+  @Delete(':id')
+  remove(@Param('id') id: number): string {
+    return `This action removes #${id} coffee.`;
+  }
 }
