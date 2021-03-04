aae3f3916202a0dcbdf9a7acb94d9453028d4634
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-04 23:49:18 +0700

    Update

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,21 +1,29 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
+  @Get()
+  findAll(@Res() res: any): any {
+    res.status(200).send('List of all coffees');
+  }
 
-    @Get()
-    findAll(@Res() res: any): any {
-        res.status(200).send('List of all coffees');
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action return #${id} coffee.`;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return `This action return #${id} coffee.`;
-    }
-
-    @Post()
-    @HttpCode(HttpStatus.GONE)
-    create(@Body() body: any): any {
-        return body;
-    }
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body: any): any {
+    return body;
+  }
 }
