4b0bd5dc66022808a2711954c13a8bd7d4eb7fc9
Frank Tran <dss_pm_se1@vtijs.com>
2021-03-04 23:49:49 +0700

    Update

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -2,19 +2,18 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
+  @Get()
+  findAll(): string {
+    return 'List of all coffees';
+  }
 
-    @Get()
-    findAll(): string {
-        return 'List of all coffees';
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action return #${id} coffee.`;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return `This action return #${id} coffee.`;
-    }
-
-    @Post()
-    create(@Body() body: any): any {
-        return body;
-    }
+  @Post()
+  create(@Body() body: any): any {
+    return body;
+  }
 }
