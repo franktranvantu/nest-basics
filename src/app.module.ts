3d14d002667d5dad750e805f0257454e3d643d03
Frank Tran <dss_pm_se1@vtijs.com>
2021-04-05 22:48:34 +0700

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule} from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    // type: 'mysql',
-    host: 'localhost',
-    // host: '172.21.3.49',
-    port: 5432,
-    // port: 3306,
-    username: 'postgres',
-    // username: 'bean',
-    password: 'frank',
-    // password: 'beandev@123',
-    database: 'nestjs',
-    // database: 'nestjs',
-    autoLoadEntities: true,
-    synchronize: true,
-  })],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: Number(process.env.PORT),
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
