cb8f236246968912a3e572a6853a2a5b7f472b79
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-04-05 16:54:48 +0700

    Update

--- src/common/dto/pagination-query.dto.ts
+++ src/common/dto/pagination-query.dto.ts
@@ -1,11 +1,14 @@
 import {IsOptional, IsPositive} from "class-validator";
+import {Type} from "class-transformer";
 
 export class PaginationQueryDto {
   @IsOptional()
   @IsPositive()
+  @Type(() => Number)
   limit: number;
 
   @IsOptional()
   @IsPositive()
+  @Type(() => Number)
   offset: number;
 }
